7c54efebfbf1825ebbeaf3db5db18c965adb08a2
Andre <andrelima.santos2370@gmail.com>
2023-10-08 14:29:24 -0300

    Adicionei pacotes separados para a main e a interface Swing que contém a lógica do jogo

---
+++ RPSGame/src/rpsgame/main/MainRPS.java
@@ -0,0 +1,14 @@
+package rpsgame.main;
+
+import RPSGame.view.TelaPrincipal;
+
+/**
+ *
+ * @author andre
+ */
+public class MainRPS {
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+}
